9fe299fb579e699372557e5a94c740d2f6a3ccb3
Lans <nothing@na.com>
2017-07-15 12:38:01 +0200

    Open New window example

---
+++ WpfApp1/WpfApp2/MyViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class MyViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        public MyViewModel(string textBoxValue)
+        {
+            TextBoxValue = textBoxValue;
+            ChooseGameCommand = new RelayCommand<object>(_ => ChooseGameCommandHandler(), _ => !string.IsNullOrEmpty(TextBoxValue));
+        }
+        
+        public MyViewModel() : this(string.Empty) { }
+
+
+        private bool _isvisible = true;
+        private string _textBoxValue;
+
+        public string TextBoxValue
+        {
+            get { return _textBoxValue; }
+            set
+            {
+                if (_textBoxValue != value)
+                {
+                    _textBoxValue = value;
+                    RaisePropertyChanged(nameof(TextBoxValue));
+                }
+            }
+        }
+        public bool IsVisible
+        {
+            get { return _isvisible; }
+            set
+            {
+                if (_isvisible != value)
+                {
+                    _isvisible = value;
+                    RaisePropertyChanged(nameof(IsVisible));
+                }
+            }
+        }
+
+        private void ChooseGameCommandHandler()
+        {
+            var window = new MainWindow();
+            window.DataContext = new MyViewModel(TextBoxValue);
+            window.Show();
+            IsVisible = false;
+        }
+
+        public ICommand ChooseGameCommand { get; private set; }        
+    }
+}
